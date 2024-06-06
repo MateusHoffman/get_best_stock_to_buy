806122558df936870ae1a39c2679cc74f278640a
Hoffman <mateus.hsilva@muu.agr.br>
2024-06-06 10:26:50 -0300

    config pwd

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,1 +1,10 @@
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// export const pwd = __dirname;
+
+
 export const pwd = "/home/hoffman/Documentos/My/get_best_stock_to_buy"
