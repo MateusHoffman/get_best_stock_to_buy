b28c0154180c470c3133d4a1b68fe0f77a8152eb
Hoffman <mateus.hsilva@muu.agr.br>
2024-06-06 00:13:42 -0300

    Implemented checkProfitLast10Years

--- src/helpers/getStockData.js
+++ src/helpers/getStockData.js
@@ -1,3 +1,4 @@
+import checkProfitLast10Years from "./CheckProfitLast10Years.js";
 import getCurrentPrice from "./GetCurrentPrice.js";
 import getDividendHistoryLast10Years from "./GetDividendHistoryLast10Years.js";
 
@@ -5,11 +6,13 @@ export default async function getStockData(ticker) {
 
   const currentPrice = await getCurrentPrice(ticker)
   const dividendHistoryLast10Years = await getDividendHistoryLast10Years(ticker)
+  const hasProfitLast10Years = await checkProfitLast10Years(ticker)
 
   const stock = {
     ticker,
     currentPrice,
     dividendHistoryLast10Years,
+    hasProfitLast10Years,
   }
   return stock
 }
